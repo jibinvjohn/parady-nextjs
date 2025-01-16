f062637710ac38cdbb87e0ef552c6684af44b428
Jibin <jibin_1@hotmail.com>
2025-01-17 00:12:53 +0530

    drizzle config update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,10 +4,10 @@ import type { Config } from 'drizzle-kit'
 export default {
   schema: './db/schema.ts',
   out: './migrations',
-  dialect: 'turso',
+  driver: 'turso',
   dbCredentials: {
-    url: process.env.TURSO_CONNECTION_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
+    url: process.env.TURSO_CONNECTION_URL || '',
+    authToken: process.env.TURSO_AUTH_TOKEN,
   },
   verbose: true,
   strict: true,
